cef67a317f6dd842c0e45a9fb7e27d784c2b358c
Shravan-LS <118159788+Shravan-LS@users.noreply.github.com>
2022-12-21 15:53:45 +0530

    Update and rename appcomponent.ts to product.js

---
+++ Advanced/Week 6/Day_13/Node Assignment - Employee Assignment/product.js
@@ -0,0 +1,10 @@
+exports.getData = function(){
+    let prodArray = [
+        {Name: "Mobile", Price: 20000, category: "A",  Quantity: 11}, 
+        {Laptop: "Laptop", Price: 60000, category:"B",  Quantity: 9} , 
+        {Name: "Printer", Price: 10000, category: "A",  Quantity: 0}, 
+        {Name: "Scanner", Price: 40000, category: "C",  Quantity: 10},
+        {Name: "Headphone", Price: 7000, category:"D",  Quantity: 7}]
+
+        return prodArray;
+}
